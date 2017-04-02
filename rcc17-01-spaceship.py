56097a657fdaf35d5e49222871bf2c078ade2406
Gregory L <levitg@gmail.com>
2017-04-02 14:06:51 +0300

    Initial commit

---
+++ rcc17-01-spaceship.py
@@ -0,0 +1,10 @@
+n = int(input())
+powers = [int(x) for x in input().split()]
+sums = 0
+for x in powers:
+    sums += x
+half = sums // 2
+for i in range(0, n - 1):
+    if powers[i] == half:
+        powers[i], powers[n - 1] = powers[n - 1], powers[i]
+print(*powers)
